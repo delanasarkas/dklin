3456e94204ef3bcef6c2fafe3237dfd73b195cf4
delana mahfiz <delsarkasmahfiz@gmail.com>
2022-08-13 14:24:27 +0700

    create login dan register page

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -36,6 +36,9 @@ $routes->set404Override();
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
+// auth
+$routes->get('/login', 'Login::index');
+$routes->get('/register', 'Register::index');
 
 /*
  * --------------------------------------------------------------------
